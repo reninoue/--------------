810ae62e7c3bd2f216104ee6423b5ba86d56d4ed
reninoue <71805701+reninoue@users.noreply.github.com>
2020-10-07 18:08:58 +0900

    test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")
   end
-
+a
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
